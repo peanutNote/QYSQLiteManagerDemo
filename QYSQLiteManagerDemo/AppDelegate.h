2ae2504d38d2a798c174a3a2b522931b1f25d5ee
qianye <422794901@qq.com>
2015-06-18 15:11:33 +0800

    Initial Commit

---
+++ QYSQLiteManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYSQLiteManagerDemo
+//
+//  Created by qianye on 15/6/18.
+//  Copyright (c) 2015年 qianye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QYSQLiteManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QYSQLiteManagerDemo
+//
+//  Created by qianye on 15/6/18.
+//  Copyright (c) 2015年 qianye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
